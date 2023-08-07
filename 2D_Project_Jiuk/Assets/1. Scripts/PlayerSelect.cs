6661a1b337e92929570468a4a111dc8e8f973664
Jiuk <wldnr1014@naver.com>
2023-08-07 21:28:38 +0900

    메탈슬러그 이미지

---
+++ 2D_Project_Jiuk/Assets/1. Scripts/PlayerSelect.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelect : MonoBehaviour
+{
+    [SerializeField] GameObject[] p1_img;
+    [SerializeField] GameObject[] Player_pick_img;
+    [SerializeField] GameObject[] Player_Select_img;
+
+
+    int picknum = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
